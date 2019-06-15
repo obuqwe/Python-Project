b8b2a1d1b4260e6e4dd1a4cef3937a6f7233d2d5
Ilya Obukhov <mesisleo10@gmail.com>
2019-06-15 11:52:22 +0300

    created countdown birthday app

---
+++ 01_Start/03_Birthday_countdown_app/countdown.py
@@ -0,0 +1,39 @@
+import datetime
+
+def print_header():
+    print('----------------------------')
+    print('       BIRTHDAY APP')
+    print('----------------------------')
+
+def get_birthday_from_user():
+    print('Tell us when you were born: ')
+    year = int(input('Year [YYYY]: '))
+    month = int(input('Month [MM]: '))
+    day = int(input('Day [DD]: '))
+
+    birthday = datetime.datetime(year, month, day)
+    return birthday
+
+def compute_days_between_dates(original_date, now):
+    date1 = now
+    date2 = datetime.datetime(now.year, original_date.month, original_date.day)
+    dt = date1 - date2
+    days = int(dt.total_seconds() / 60 / 60 / 24)
+    return days
+
+def print_birthday_information(days):
+    if days < 0:
+        print(f'Your birthday is in {-days} days!')
+    elif days > 0:
+        print(f'You had your birthday already this year! {days} days ago.')
+    else:
+        print('Happy birthday!')
+
+def main():
+    print_header()
+    bday = get_birthday_from_user()
+    now = datetime.datetime.now()
+    number_of_days = compute_days_between_dates(bday, now)
+    print_birthday_information(number_of_days)
+
+main()
